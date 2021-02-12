144a89b9b314fc9022c8c81b4bffaaebf59d25ec
Jonathan <jonathanjampamb@gmail.com>
2021-02-12 18:11:55 -0300

    identified adm

--- controller/cadastro.php
+++ controller/cadastro.php
@@ -2,7 +2,20 @@
     require("conexao.php");
 
     if(isset($_POST['email']) && isset($_POST['senha'])){
-        echo "senha bem Vindo ".$_POST['email'];
+        $query = $conn->prepare("SELECT * FROM Tb_contrato WHERE email = ? AND senha = ?");
+        $query->execute(array($_POST['email'], $_POST['senha']));
+
+        // echo $query->rowCount();
+
+        if($query->rowCount()){
+            $usuario = $query->fetchAll(PDO::FETCH_ASSOC)[0];
+
+            print_r($usuario);
+        }else{
+            echo "Erro";
+        }
+    }else{
+        echo "não tem Autorização";
     }
 
 ?>
